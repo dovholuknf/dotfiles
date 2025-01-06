dc57a2d150b53941b9fa97c39fe6c68b7605c9c9
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-01-06 09:20:52 -0500

    check for powershell > 7

--- powershell/onpath/resetwsl.ps1
+++ powershell/onpath/resetwsl.ps1
@@ -1,3 +1,11 @@
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+    "PowerShell version 7 or higher installed. Continuing..."
+} else {
+    Write-Host -ForegroundColor Red "PowerShell version is below 7. Use PowerShell v7+"
+    Write-Host -ForegroundColor Red "Current PowerShell version: $($PSVersionTable.PSVersion)"
+    exit
+}
+
 wsl --shutdown
 wsl --mount V:\work\wsl\100gb-dev-mar-2024.vhdx --vhd --name dev
 
